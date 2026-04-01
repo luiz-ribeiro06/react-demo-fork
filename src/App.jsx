ccf8bbba92b8204b6d4d2a0f54887ba017a7b9c8
luiz-ribeiro06 <luiz.ribeiro06@aluno.ifce.edu.br>
2026-04-01 16:49:22 -0300

    Change header text to new game title

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Eu acho que todo mundo deveria ficar com 10</h1>
+      <h1>My Bunny Friend will be the best game ever made</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
